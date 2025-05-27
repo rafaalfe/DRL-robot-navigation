9aabc827a08a4ab19dcead91b44f6042475d64e6
rafaalfe <rafaalfetinno@gmail.com>
2025-05-27 22:04:53 +0700

    update 27.1

---
+++ realsense/build/realsense-ros/realsense2_camera/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot/realsense/src/realsense-ros/realsense2_camera
+# Build directory: /home/robot/realsense/build/realsense-ros/realsense2_camera
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ realsense/build/realsense-ros/realsense_gazebo_plugin/CMakeFiles/realsense_gazebo_plugin.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/robot/realsense/src/realsense-ros/realsense_gazebo_plugin/src/RealSensePlugin.cpp" "realsense-ros/realsense_gazebo_plugin/CMakeFiles/realsense_gazebo_plugin.dir/src/RealSensePlugin.cpp.o" "gcc" "realsense-ros/realsense_gazebo_plugin/CMakeFiles/realsense_gazebo_plugin.dir/src/RealSensePlugin.cpp.o.d"
+  "/home/robot/realsense/src/realsense-ros/realsense_gazebo_plugin/src/gazebo_ros_realsense.cpp" "realsense-ros/realsense_gazebo_plugin/CMakeFiles/realsense_gazebo_plugin.dir/src/gazebo_ros_realsense.cpp.o" "gcc" "realsense-ros/realsense_gazebo_plugin/CMakeFiles/realsense_gazebo_plugin.dir/src/gazebo_ros_realsense.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
